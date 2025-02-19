0a3c381e4e2487cde305c8925c6283efcfead75e
JadahH <Jhol1918@students.vsu.edu>
2025-02-19 18:49:48 -0500

    Added test to caculate the min,max and avg

--- Int.Tests/GetArrayTest.cs
+++ Int.Tests/GetArrayTest.cs
@@ -2,3 +2,16 @@ using System.Linq;
 using Xunit;
 
 namespace Int.Tests;
+
+public class GetArrayTests
+    {
+        [Fact]
+        public void GetArray_ShouldReturnCorrectStatistics()
+        {
+            int[] numbers = Enumerable.Range(1, 100).ToArray();
+            var stats = GetArray.GetStatistics(numbers);
+            Assert.Equal(50.5, stats.Average);
+            Assert.Equal(1, stats.Min);
+            Assert.Equal(100, stats.Max);
+        }
+    }
