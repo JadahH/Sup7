3f8d6580a6f90b3726c248b5146de19194c961d7
JadahH <Jhol1918@students.vsu.edu>
2025-02-19 18:32:49 -0500

    Implemented LINQ method to filter even number and added option to skip numbers

--- Int/GetEven.cs
+++ Int/GetEven.cs
@@ -3,4 +3,15 @@
 public class GetEven
 {
 
+        /// <summary>
+        /// Returns all even numbers from an array with an option to skip numbers.
+        /// </summary>
+        /// <param name="numbers">The array of integers</param>
+        /// <param name="skip">Optional number of items to skip</param>
+        /// <returns>Enumerable collection of even numbers</returns>
+        public static IEnumerable<int> GetEvenNumbers(int[] numbers, int skip = 0)
+        {
+            return numbers.Where(n => n % 2 == 0).Skip(skip);
+ 
+        }
 }
